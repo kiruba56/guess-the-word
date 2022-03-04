64ac3bf5270cc9eea66fd85a2ff8afbef74cffb9
kiruba <darkpro529@gmail.com>
2022-03-04 21:55:52 +0530

    btn

---
+++ src/components/bouncy.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View} from 'react-native';
+
+interface Props {
+    
+}
+
+const Bouncy:React.FC<Props> = () => {
+    return (
+        <View></View>
+    )
+};
