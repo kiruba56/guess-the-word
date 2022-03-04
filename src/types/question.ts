fbcaefb60742710c8c9541f3ca01a18fffdcf577
kiruba <darkpro529@gmail.com>
2022-03-05 01:53:08 +0530

    lil cmnt

--- src/types/question.ts
+++ src/types/question.ts
@@ -1,14 +1,19 @@
 import { word } from "./word";
 
 export interface Question {
+    // The sentence in english
     example_sentence:string[],
+    // 3 random words to shuffle with right answer
     random_words:string[],
+    // The sentence in german including the answer
     sentence:word[],
     qustion_index:Index
 }
 
 
 interface Index {
+    // the index of the the word that need to be highlighted in example sentence
     example:number,
+    // the index of the word that user need to guess
     sentence:number
 };
